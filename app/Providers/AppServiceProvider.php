903615a423661d067fddf6dabf33e6f11511c74f
GuiCesar12 <guilherme.cesar.reis@hotmail.com>
2023-10-30 20:31:47 -0300

    Funcionando

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->register(\Ripcord\Providers\Laravel\ServiceProvider::class);
+        //
     }
 
     /**
